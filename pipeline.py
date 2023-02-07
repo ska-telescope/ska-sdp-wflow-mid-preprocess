331c3c5cb6456a5f82065b78203daf60df7cc761
shahriar_etemadi_tajbakhsh <shahriar.etemaditajbakhsh@eng.ox.ac.uk>
2023-02-07 15:20:23 +0000

    HIP-464, prefalgger added to the pipeline

--- pipeline.py
+++ pipeline.py
@@ -2,10 +2,12 @@ import numpy as np
 import dp3
 import argparse
 
-
 parser = argparse.ArgumentParser(description='extracted data location')
 parser.add_argument('--dataloc', type=str, nargs=1, help='the location of data files')
+
+
 args = parser.parse_args()
+
 data_dir = args.dataloc
 
 parset = dp3.parameterset.ParameterSet()
@@ -26,25 +28,24 @@ flag_loc = data_dir[0] + "/flag.npy"
 mask_loc = data_dir[0] + "/rfi_mask.npy"
 
 
-vis = np.load(vis_loc)
-vis_dims = vis.shape
+vis_ms = np.load(vis_loc)
+flags_ms = np.load(flag_loc)
+
+
 
 chan_freq = np.load(chan_freq_loc)
 chan_width = np.load(chan_width_loc)
 
+vis_ms_dims = vis_ms.shape
+print(vis_ms_dims)
 
-
-print(vis_dims)
-
-num_correlations = vis_dims[2] 
+num_correlations = vis_ms_dims[2] 
 
 
 
 dpinfo = dp3.DPInfo(num_correlations)
 
 dpinfo.set_channels(chan_freq, chan_width)
-print("frequencies: ", dpinfo.channel_frequencies)
-print("widths: ", dpinfo.channel_widths)
 
 
 ant_name = np.load(ant_name_loc)
@@ -52,12 +53,49 @@ ant_pos = np.load(ant_pos_loc)
 ant_diameter = np.load(ant_diameter_loc)
 antenna1 = np.load(antenna1_loc)
 antenna2 = np.load(antenna2_loc)
+num_ants = len(ant_name)
+num_baselines = int(num_ants * (num_ants + 1)/2)
+num_freqs = len(chan_freq)
+num_times = int(vis_ms.shape[0]/num_baselines)
+print(num_times)
 
 rfi_mask = np.load(mask_loc)
-print(rfi_mask)
+chan = np.array([])
+
+vis = vis_ms.reshape([num_times, num_baselines, num_freqs, num_correlations])
+flags = flags_ms.reshape([num_times, num_baselines, num_freqs, num_correlations])
+
+for freq in range(num_freqs):
+    if rfi_mask[freq]:
+       chan = np.append(chan, freq)
+
+
+parset.add("preflagger.chan", str(chan))
+
 
 dpinfo.set_antennas(ant_name, ant_diameter, ant_pos, antenna1, antenna2)
 
-print(dpinfo.antenna_names)
-print(dpinfo.antenna_positions)
+preflagger_step = dp3.make_step("preflagger", parset,"preflagger.", dp3.MsType.regular)
+null_step = dp3.make_step("null", parset, "", dp3.MsType.regular)
+preflagger_step.set_next_step(null_step) 
+
+def flags_ratio(flags):
+    tot = flags.shape[0] * flags.shape[1] * flags.shape[2]  
+    flag_sum = np.sum(np.sum(np.sum(np.sum(flags))))
+    return 100 * (flag_sum/tot)
+
+for time in range(num_times):
+    dpbuffer = dp3.DPBuffer()
+    print(flags_ratio(flags[time, :, :, :]))
+    dpbuffer.set_flags(flags[time, :, :, :])
+    preflagger_step.process(dpbuffer)
+    myflags = dpbuffer.get_flags()
+
+
+
+
+
+
+
+
 
